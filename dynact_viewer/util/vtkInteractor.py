e01726e55d0b58e8fa0f1cc6903ca44ae2d6b505
Michael Kuczynski <mkuczyns@ucalgary.ca>
2020-02-05 10:33:22 -0700

    Added comments

--- dynact_viewer/util/vtkInteractor.py
+++ dynact_viewer/util/vtkInteractor.py
@@ -16,6 +16,7 @@ class MyInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
 
     def RightButtonPressEvent(self, obj, event):     
         
+        # Get the point picked by the right mouse button
         self.GetInteractor().GetPicker().Pick( self.GetInteractor().GetEventPosition()[0], 
                                                self.GetInteractor().GetEventPosition()[1], 
                                                0, 
@@ -24,21 +25,41 @@ class MyInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
 
         point = self.GetInteractor().GetPicker().GetPickPosition()
 
-        print(f'Picking point:  {point}')
+        # Get the 
+        actorList = self.GetInteractor().GetPicker().GetActors()
 
-        # Draw a sphere at the picked point
-        source = vtk.vtkSphereSource()
-        source.SetRadius(1.0)
-        source.SetCenter(point[0], point[1], point[2])
+        if actorList.GetNumberOfItems() :
+            # Get polyData from actor
+            nextActor = actorList.GetLastActor() # Get the next actor
+            nextMapper = nextActor.GetMapper()   # Get the next actor's PolyData mapper
+            polyData = nextMapper.GetInput()     # Get the next actor's PolyData
 
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(source.GetOutputPort())
+            # Draw a sphere at the picked point
+            source = vtk.vtkSphereSource()
+            source.SetRadius(1.0)
+            source.SetCenter(point[0], point[1], point[2])
 
-        actor = vtk.vtkActor()
-        actor.GetProperty().SetDiffuseColor(1,0,0)
-        actor.SetMapper(mapper)
+            # Check if the picked point is inside the surface
+            selectEnclosedPoints = vtk.vtkSelectEnclosedPoints()
+            selectEnclosedPoints.SetInputData( source.GetOutput() )
+            selectEnclosedPoints.SetSurfaceData( polyData )
+            selectEnclosedPoints.SetTolerance(0.000001)     # Reduce the tolerance so we can pick points easier
+            selectEnclosedPoints.Update()
 
-        self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().AddActor(actor)
+            val = selectEnclosedPoints.IsInsideSurface( point )
+
+            # If the picked point is within the PolyData surface, draw it
+            if val :
+                print(f'Picking point:  {point}')
+
+                mapper = vtk.vtkPolyDataMapper()
+                mapper.SetInputConnection(source.GetOutputPort())
+
+                actor = vtk.vtkActor()
+                actor.GetProperty().SetDiffuseColor(1,0,0)
+                actor.SetMapper(mapper)
+
+                self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().AddActor(actor)
         
         self.OnRightButtonDown()
         
